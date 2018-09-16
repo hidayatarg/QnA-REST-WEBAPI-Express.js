f1c94644c4af7154eb2fbc5349b72c41aa3647ab
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-16 13:09:15 +0300

    Accepting Data by middleware

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 'use strict';
 var express=require('express');
 var app=express();
+var jsonParser= require('body-parser').json;
 
-app.use(function (req, res, next){
+var jsonChecker=function (req, res, next){
+ if (req.body) {
+     console.log('The Sky is', req.body.color);
+ } else {
+     console.log('There is no body property on the request');
+ }
+ next();
+}
 
-});
+app.use(jsonChecker);
+
+//when app recieve a request this middleware will
+//the body. make it accessable from the request body property
+app.use(jsonParser());
+
+app.use(jsonChecker);
 
 var port=process.env.PORT || 3000;
 
