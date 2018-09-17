7cdddf65cada1f8b207c3a31242c6399c372a142
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-17 14:25:13 +0300

    Extending Mongoose

--- models/model.js
+++ models/model.js
@@ -3,11 +3,43 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
+var sortAnswers = function (a,b){
+    // - negative b before b
+    // 0 no change
+    // + positive a after b
+    if(a.votes===b.votes){
+        // if(a.updatedAt>b.updatedAt){
+        //     return -1;
+        // }else if(a.updatedAt<b.updatedAt){
+        //     return +1;
+        // }else{
+        //     return 0;
+        // }
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes
+}
+
 var AnswerSchema = new Schema({
         text:String,
         createdAt:{type: Date, default: Date.now},
         updatedAt:{type: Date, default: Date.now},
         votes:{type: Number, default:0}
+});
+
+// Answer is a sub document
+AnswerSchema.methods('update', function(updates,callback){
+    Object.assign(this, updates, {updatedAt:new Date()});
+    this.parent().save(callback);
+});
+
+AnswerSchema.methods('vote', function(vote,callback){
+    if(vote==='up'){
+        this.votes+=1;
+    }else{
+        this.votes-=1;
+    }
+    this.parent().save(callback);
 });
 
 var QuestionSchema = new Schema({
@@ -16,6 +48,10 @@ var QuestionSchema = new Schema({
         answers:[AnswerSchema]
 });
 
+QuestionSchema.pre('save',function(next){
+    this.answer.sort(sortAnswers);
+    next();
+})
 
 var Question = mongoose.model('Question', QuestionSchema);
 module.exports.Question = Question;
