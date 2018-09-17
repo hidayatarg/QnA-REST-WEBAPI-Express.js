cdda49994d98489223ba74c75258c49a78ab785b
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-17 15:52:00 +0300

    Answer PUT, Delete routes Implemented

--- routes.js
+++ routes.js
@@ -4,6 +4,36 @@ var express = require('express');
 var router = express.Router();
 var Question= require('./models/models').Question;
 
+// for specific question route
+// callback is execu. ??qID present??
+router.params('qID',function(req, res, next,id){
+    Question.findById(id, function (err, doc) {
+        if (err) return next(err);
+        // If document not found
+        if(!doc){
+            err = new Error("Not Found");
+            err.status=404;
+            return next(err);
+        }
+        req.question=doc;
+        return next();
+
+    });
+});
+
+// for specific answer route
+router.params('aID', function(req, res, next, id){
+    req.answer=req.question.answers.id(id);
+    if(!req.answer){
+        err = new Error("Not Found");
+        err.status=404;
+        return next(err);
+    }
+    // To pass control back to router
+    next();
+
+});
+
 //GET /questions
 //Function route handler
 //Return all the questions
@@ -37,43 +67,40 @@ router.post('/', function(req, res, next){
 //GET /questions/:id
 //Route for specific questions
 router.get('/:id', function(req, res, next){
-    Question.findById(req.params.qID, function(err, doc){
-        if(err) return next(err);
-        res.json(doc);
-
-    });
+    res.json(req.question);
 });
 
 //POST /questions/:qID/answers/:aID
 //Route for creating answer
-router.post('/:qID/answers', function(req, res){
-    res.json({
-        response:"You Sent a POST request for /answers",
-        questionId:req.params.qID,
-        body:req.body
-    });
+router.post('/:qID/answers', function(req, res, next){
+    req.question.answer.push(req.body);
+    req.question.save(function (err, question) {
+        if (err) return next(err);
+        err.status(201);
+        //return document as json to client
+        res.json(question);
+    });  
 });
 
 //PUT /questions/:id/answers
 //Route for Editing a specific answer
 router.put('/:qID/answers/:aID', function(req, res){
-    res.json({
-        response:"You Sent a PUT request for /answers",
-        questionId: req.params.qID,
-        answerId: req.params.aID,
-        body:req.body
-    });
+    // Answer is Loaded
+    req.answer.update(req.body, function(err, result){
+        if(err) return next(err);
+        res.json(result)
+    });  
 });
 
 //DELETE /questions/:qID/answers/:aID
 //Route for Deleting a specific answer
 router.delete('/:qID/answers/:aID', function(req, res){
-    res.json({
-        response:"You Sent a DELETE request for /answers",
-        questionId: req.params.qID,
-        answerId: req.params.aID,
-        body:req.body
-    });
+   req.answer.remove(function (err){
+       req.question.save(function(err, question){
+            if(err) return next(err);
+            res.json(question);
+       });
+   });
 });
 
 //POST /questions//:qID/answers/:aID/vote-up
